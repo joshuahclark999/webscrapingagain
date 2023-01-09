6ec72e77faf50cee0efc76e7aed1511da007eaee
Joshuah Clark <69828377+joshuahclark999@users.noreply.github.com>
2023-01-09 11:33:45 -0500

    fixes

--- webscrapinggain.py
+++ webscrapinggain.py
@@ -5,31 +5,32 @@ import re
 import random
 import time 
 
-
 R  = '\033[31m' # red
 G  = '\033[32m' # green
 
 def extractSource(username):
     useragents = open('useragents.txt').read().splitlines()
-
+  
     with open('websitelist.txt') as f:
         for site in f:
           site = site.strip()
-          site = site + username
           useragent = random.choice(useragents)
           headers = {
-            "User-Agent": useragent
-          }
+              "User-Agent": useragent
+            }
+          site = site + username
           r = requests.get(site, headers=headers)
-          if r.status_code == 200:
-            match = re.search(username, r.text)
-            if match != "":
-              print(G +"Account found on: " + site+ G)
-              time.sleep(3)
-          elif r.status_code != 200:
-              print(R+"Account not found on: " + site + "[Status Code: " + str(r.status_code) +"]"+R)
-              time.sleep(3)
-              
+          parseHTML(r, username, site)
+                      
+def parseHTML(request, username,site): 
+  if request.status_code == 200:
+    match = re.search(username, request.text)
+    if match != "":
+        print(G +"Account found on: " + site+ G)
+        time.sleep(3)
+    elif request.status_code != 200:
+        print(R+"Account not found on: " + site + "[Status Code: " + str(request.status_code) +"]"+R)
+        time.sleep(3)
 
 def main():
   
